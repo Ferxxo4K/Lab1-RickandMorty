6faad784b195527763e3466e84270f3a2a381fad
JUNIOR <ferneyagu04@gmail.com>
2024-10-22 02:26:42 -0500

    Versionstable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filters from "./Components/Filters/Filters";
+import Cards from "./Components/Cards/Cards";
+import Pagination from "./Components/Pagination/Pagination";
+import Search from "./Components/Search/Search";
+import Navbar from "./Components/Navbar/Navbar";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
 
 function App() {
   return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/episodes" element={<Episodes />}/>
+      <Route path="/location" element={<Location />}/>
+      </Routes> 
+    </Router>
+  );
+};
+
+const Home = () => {
+
+  let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+  let [status, setStatus] = useState("");
+  let [gender, setGender] = useState("");
+  let [species, setSpecies] = useState("");
+
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+
+  let Urlinicial = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(Urlinicial).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [Urlinicial]);
+
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center ubuntu my-4">
+        Rick and Morty <span className="text-primary">By Ferney</span>
+      </h1>
+
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+
+      <div className="container">
+        <div className="row">
+          <Filters
+            setSpecies={setSpecies}
+            setGender={setGender}
+            setStatus={setStatus}
+            setPageNumber={setPageNumber}
+
+          />
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/Location.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Location = () => {
+    return <div>Hi! que gusto verte aqui, actualmente esta opción no se encuentra diponible</div>;
+};
+
+export default Location
